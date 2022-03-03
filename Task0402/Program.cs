2e7e8edaa76faa1e56562c466e3813fe1a9c9e03
Vitaliy <viurum986@mail.ru>
2022-03-03 13:37:35 +0300

    Task0402

---
+++ Task0402/Program.cs
@@ -0,0 +1,11 @@
+﻿int a; 
+Console.WriteLine("Введите число ");
+a = int.Parse(Console.ReadLine());
+if (a <= 5)
+{
+    Console.WriteLine("Рабочий день");
+}
+else
+{
+    Console.WriteLine("Выходной день");
+}
